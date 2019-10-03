da64a477e5ea5f6bf6bc8006c225819c1db041e8
tavisca-mswarnesh <mswarnesh@tavisca.in>
2019-10-03 10:58:01 +0530

    Initial website

---
+++ MyWebsite/Models/Details.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebsite.Models
+{
+    public class Details
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
